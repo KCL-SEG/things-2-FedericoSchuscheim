397eb48d904a73d3b9d62d4bd588431b32f226c3
Federico Schuscheim <federico.schuscheim@kcl.ac.uk>
2023-12-08 14:55:04 +0000

    Add form to view

--- things/views.py
+++ things/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse
+from things.forms import ThingForm
+from django.views.generic import FormView
 
-def home(request):
-    return render(request, 'home.html', {'form': form})
+class HomeView(FormView):
+    form_class = ThingForm
+    template_name = 'home.html'
+    
+    def form_valid(self, form: ThingForm):
+        """Handle valid form by saving the new thing."""
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Return redirect URL after successful team creation."""
+        return reverse('home')
