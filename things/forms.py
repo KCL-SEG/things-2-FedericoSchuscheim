def2e514cd3da17ba7f47c5ea1e107a611b29919
Federico Schuscheim <federico.schuscheim@kcl.ac.uk>
2023-12-08 14:40:25 +0000

    Create form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django.forms import ModelForm, Textarea
+from .models import Thing
+
+
+class ThingsForm(ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': Textarea()}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def save(self):
+        super().save(commit=False)
+        thingObject = Thing.objects.create(
+            name=self.cleaned_data.get('name'),
+            description=self.cleaned_data.get('description'),
+            quantity = self.cleaned_data.get('quantity'),
+        )
+        return thingObject
